b4e7f1b012891ac4976e5b162b34ea806168c278
mzaidiii <mohdmurtaza153@gmail.com>
2026-06-12 23:59:42 +0530

    all the api related to Login page

---
+++ lib/core/networks/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'https://pawffy-backend.onrender.com';
+
+  static const String login = '/api/auth/login';
+  static const String register = '/api/auth/register';
+  static const String me = '/api/auth/me';
+}
