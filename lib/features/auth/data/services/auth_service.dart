ec0c634b003a85a02b83005a9b46549221ed0066
mzaidiii <mohdmurtaza153@gmail.com>
2026-06-13 11:01:55 +0530

    Api Layer of the application

---
+++ lib/features/auth/data/services/auth_service.dart
@@ -0,0 +1,78 @@
+import 'package:dio/dio.dart';
+
+import 'package:pawffy/core/networks/dio_client.dart';
+import 'package:pawffy/core/networks/api_constants.dart';
+import '../models/auth_response_model.dart';
+import '../models/user_model.dart';
+
+class AuthService {
+  final Dio _dio = DioClient.dio;
+
+  Future<AuthResponseModel> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await _dio.post(
+        ApiConstants.login,
+        data: {'email': email, 'password': password},
+      );
+
+      return AuthResponseModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Login failed');
+    }
+  }
+
+  Future<AuthResponseModel> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await _dio.post(
+        ApiConstants.register,
+        data: {'name': name, 'email': email, 'password': password},
+      );
+
+      return AuthResponseModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Registration failed');
+    }
+  }
+
+  Future<UserModel> getMe(String token) async {
+    try {
+      final response = await _dio.get(
+        ApiConstants.me,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+
+      return UserModel.fromJson(response.data['data']);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Failed to fetch profile');
+    }
+  }
+
+  Future<UserModel> updateProfile({
+    required String name,
+    required String phone,
+    required String city,
+    required String state,
+    required String token,
+  }) async {
+    try {
+      final response = await _dio.put(
+        '/api/users/me',
+        data: {'name': name, 'phone': phone, 'city': city, 'state': state},
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+
+      return UserModel.fromJson(response.data['data']);
+    } on DioException catch (e) {
+      throw Exception(
+        e.response?.data['message'] ?? 'Failed to update profile',
+      );
+    }
+  }
+}
