a387ae2264aaffad85650a3795408208afa69753
mzaidiii <mohdmurtaza153@gmail.com>
2026-06-14 21:24:39 +0530

    file to display the user name on home screen

---
+++ lib/features/auth/providers/current_user_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pawffy/features/auth/data/models/user_model.dart';
+import 'package:pawffy/features/auth/providers/auth_controller.dart';
+
+final currentUserProvider =
+    AsyncNotifierProvider<CurrentUserNotifier, UserModel?>(
+      CurrentUserNotifier.new,
+    );
+
+class CurrentUserNotifier extends AsyncNotifier<UserModel?> {
+  @override
+  Future<UserModel?> build() async {
+    return await ref.read(authControllerProvider.notifier).getMe();
+  }
+
+  void clear() {
+    state = const AsyncData(null);
+  }
+}
