59a89c3e44297e9c21c257bd5a71c1286203b521
mzaidiii <mohdmurtaza153@gmail.com>
2026-06-15 23:08:56 +0530

    Adding the Notification and the pet apis

--- lib/core/networks/api_constants.dart
+++ lib/core/networks/api_constants.dart
@@ -6,4 +6,13 @@ class ApiConstants {
   static const String me = '/api/auth/me';
   static const String updateMe = '/api/users/me';
   static const String vets = '/api/vets';
+  static const String notifications = '/api/notifications';
+  static const String markAllNotificationsRead = '/api/notifications/read-all';
+
+  static String markNotificationRead(String id) =>
+      '/api/notifications/$id/read';
+
+  static String deleteNotification(String id) => '/api/notifications/$id';
+  static const String pets = '/api/pets';
+  static String petById(String id) => '/api/pets/$id';
 }
