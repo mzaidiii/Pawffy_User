b6e8268ea55ebafe48334d5d36b9a6088ce034bc
mzaidiii <mohdmurtaza153@gmail.com>
2026-06-20 18:13:36 +0530

    Fetching real time Providers List

--- lib/features/pets/providers/pet_provider.dart
+++ lib/features/pets/providers/pet_provider.dart
@@ -1,4 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../data/services/pet_service.dart';
+import '../data/services/medical_record_service.dart';
+import '../data/services/vaccination_service.dart';
 
 final petServiceProvider = Provider<PetService>((ref) => PetService());
+final medicalRecordServiceProvider = Provider<MedicalRecordService>((ref) => MedicalRecordService());
+final vaccinationServiceProvider = Provider<VaccinationService>((ref) => VaccinationService());
+
